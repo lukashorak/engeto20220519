8d68c7fcf8b3b658dba3e80d86e0f3fd0691ec25
Lukas Horak <lukas.horak@gmail.com>
2022-05-19 20:00:39 +0200

    Initial commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testFirst(){
+        System.out.println("My First Test");
+    }
+
+    @Test
+    public void testAdd(){
+        int sum = HelloWorld.add(3,5);
+        Assertions.assertEquals(8, sum);
+    }
+
+    @Test
+    public void testAdd2(){
+        int sum = HelloWorld.add(3,5);
+        Assertions.assertNotEquals(7, sum);
+    }
+
+    @Test
+    public void testError() throws Exception {
+        Assertions.assertThrows(Exception.class, ()->{
+            throw new Exception("Some error");
+        });
+    }
+}
